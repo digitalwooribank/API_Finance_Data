6c094a5fc3f0bda1bc82e9335b43e8a2075014e8
blueseam <blueseam@gmail.com>
2019-06-28 19:28:06 +0900

    Update Urldefine.java

--- Android_example/app/src/main/java/kr/co/itmsg/woori/define/Urldefine.java
+++ Android_example/app/src/main/java/kr/co/itmsg/woori/define/Urldefine.java
@@ -1,7 +1,7 @@
 package kr.co.itmsg.woori.define;
 
 public class Urldefine {
-    public static String URL = "https://15.164.140.220:8443";
+    public static String URL = "https://10.0.0.1:8443";
     public static String getAccTransList = "/oai/wb/v1/finance/getAccTransList";            //거래내역조회
     public static String getIndivAllAccInfo="/oai/wb/v1/finance/getIndivAllAccInfo";        //전계좌조회
     public static String getAccBasicInfo = "/oai/wb/v1/finance/getAccBasicInfo";            //계좌기본조회
